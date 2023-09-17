acc528d8b38efd50eb28d265cb4efc4016f1c652
大海一余 <brilliant20119@live.com>
2023-09-17 18:25:17 +0800

    整合 tailwind css

--- src/pages/frontend/index.vue
+++ src/pages/frontend/index.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>首页</h1>
+  <h1>首页</h1>
+  <div class="bg-green-300 inline">绿色</div>
+  <div class="bg-yellow-300 ml-2 inline">黄色</div>
+  <div class="bg-blue-300 ml-2 inline">蓝色</div>
+
+  <div class="text-xs sm:text-lg md:text-xl lg:text-2xl">响应式字体</div>
 </template>
