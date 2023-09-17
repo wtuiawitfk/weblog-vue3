c6a39e707739afa500d97b1c6fbd910aecf3e4bb
大海一余 <brilliant20119@live.com>
2023-09-17 17:55:30 +0800

    搭建前端框架

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from "vue"; // 引入 createApp 方法
 import App from "./App.vue"; // 引入 App.vue 组件
-
-import "./assets/main.css"; // 引入 main.css 样式文件
+// 导入路由
+import router from "./router";
+// import "./assets/main.css"; // 引入 main.css 样式文件
 
 // 创建应用，并将 App 根组件挂载到 <div id="#app"></div> 中
-createApp(App).mount("#app");
+// createApp(App).mount("#app");
+
+// 应用路由
+const app=createApp(App)
+
+app.use(router)
+app.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Index from '../pages/frontend/index.vue'
+import { createRouter,createWebHashHistory } from 'vue-router'
+
+// 统一声明所有路由
+const routes=[
+  {
+    path:'/',
+    component:Index,
+    meta:{
+      title:'weblog 首页'
+    }
+  }
+]
+
+// 创建路由
+const router=createRouter({
+  // 指定路由的历史管理方式，hash 模式指的是 URL 的路径是通过 hash 符号（#）进行标识
+  history:createWebHashHistory(),
+  // routes: routes 的缩写
+  routes,
+})
+
+// 导出
+export default router
