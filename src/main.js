525ebaae8fe1eb43aba9d524dffcfe759e5a2990
大海一余 <brilliant20119@live.com>
2023-09-24 09:29:39 +0800

    登录页设计

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import { createApp } from "vue"; // 引入 createApp 方法
-import App from "@/App.vue"; // 引入 App.vue 组件
-// 导入路由
-import router from "@/router";
-import "./assets/main.css"; // 引入 main.css 样式文件
+import '@/assets/main.css'
 
-// 创建应用，并将 App 根组件挂载到 <div id="#app"></div> 中
-// createApp(App).mount("#app");
+import { createApp } from 'vue'
+import App from '@/App.vue'
+// 导入路由
+import router from '@/router'
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
 
 // 应用路由
-const app=createApp(App)
-
 app.use(router)
+
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
 app.mount('#app')
